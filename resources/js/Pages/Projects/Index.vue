456f2d6644035eb154c6f3f7b92270337884695b
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-26 07:59:01 +0700

    feat: Add link for Create Page

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -8,7 +8,13 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">Table</div>
+                    <div class="p-6 text-gray-900">
+                        <div class="flex justify-end m-2 p-2">
+                            <Link href="/projects/create" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md"> 
+                                New Project
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,5 +23,5 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import { Head, Link } from '@inertiajs/inertia-vue3';
 </script>
--- resources/js/Pages/Skills/Create.vue
+++ resources/js/Pages/Skills/Create.vue
@@ -1,19 +1,19 @@
 <template>
-    <Head title="Skills Index" />
+    <Head title="New Skill" />
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Skills</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">New Skill</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        <div class="flex justify-end m-2 p-2">
+                        <!-- <div class="flex justify-end m-2 p-2">
                             <Link :href="route('skills.create')" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md"> 
                                 New Skill
                             </Link>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -23,5 +23,5 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import { Head } from '@inertiajs/inertia-vue3';
 </script>
