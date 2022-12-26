03aad61dc0f45b18c6edc5f001449ab60be9c90f
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-26 07:59:30 +0700

    refactor: synchronize code so it can run

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -24,7 +24,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Projects/Create');
     }
 
     /**
--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -24,7 +24,7 @@ class SkillController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Skills/Create');
     }
 
     /**
